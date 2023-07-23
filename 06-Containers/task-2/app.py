beba4c81576952fa14c2739d34bc0eb1b369a6a2
Dkorchovyi <dmitriy.korchovuy@gmail.com>
2023-07-24 01:07:52 +0300

    task-2 done

---
+++ 06-Containers/task-2/app.py
@@ -0,0 +1,10 @@
+import time
+
+def print_current_time():
+    while True:
+        current_time = time.strftime("%Y-%m-%d %H:%M:%S")
+        print("Поточний час і дата:", current_time)
+        time.sleep(10)
+
+if __name__ == "__main__":
+    print_current_time()
