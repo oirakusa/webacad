c73be9cd3e06fd8e91b24a2473fc324f8fef3624
Dkorchovyi <dmitriy.korchovuy@gmail.com>
2023-07-25 00:33:58 +0300

    fixed task-2 redis-flask connection

--- 06-Containers/task-1/run.sh
+++ 06-Containers/task-1/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
-docker run -d -v /redis-data/:/data --name redis wa-redis
-docker run -d -p --name flask 8080:8080 wa-flask
+docker network create flask
+docker run -d -v /redis-data/:/data --network flask --name redis wa-redis
+docker run -d -p --network flask --name flask 8080:8080 wa-flask
